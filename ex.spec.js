4612ca29cea22ec3137d349d036b4270417554d2
ss1864510 <shaiksubhani.basheer@gmail.com>
2026-07-05 16:32:41 +0530

    second

--- ex.spec.js
+++ ex.spec.js
@@ -4,4 +4,6 @@ test('homepage has correct title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveScreenshot('homepage.png',{fullPage:true});
+
 });
