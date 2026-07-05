e3717a8953eca86ed7cbfa610ba8b9e75154fd07
ss1864510 <shaiksubhani.basheer@gmail.com>
2026-07-05 16:55:25 +0530

    second2

--- ex.spec.js
+++ ex.spec.js
@@ -4,6 +4,8 @@ test('homepage has correct title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   await expect(page).toHaveTitle(/Playwright/);
+   await page.screenshot({ path: 'homepage.png', fullPage: true }); 
   await expect(page).toHaveScreenshot('homepage.png',{fullPage:true});
+  await expect(page.getByText('Docs')).toBeVisible();
 
 });
